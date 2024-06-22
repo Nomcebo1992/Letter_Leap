1f4835cbb111b51debb47102b2d24dcec0427a94
Nomcebo1992 <snembambo@gmail.com>
2024-06-21 17:40:16 -0700

    api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,22 +2,54 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-router.get("/greeting", (req, res) => {
-  res.json({ message: "Hello from the server!" });
+// Retrieve a user's progress
+router.get("/users/:username/progress", (req, res) => {
+  const { username } = req.params;
+  User.findOne({ username }, (err, user) => {
+    if (err) return res.status(500).send(err);
+    if (!user) return res.status(404).send({ message: "User not found" });
+    return res.send(user.progress);
+  });
 });
 
-router.post("/update-progress", (req, res) => {
-  const { username, progress } = req.body;
-
+// Update a user's progress for a specific letter
+router.patch("/users/:username/progress/letters/:letter", (req, res) => {
+  const { username } = req.params;
+  const { letter } = req.params;
+  const { value } = req.body;
   User.findOneAndUpdate(
     { username },
-    { $set: { progress } },
-    { new: true, upsert: true },
+    { $set: { [`progress.alphabet.${letter}`]: value } },
+    { new: true },
     (err, user) => {
       if (err) return res.status(500).send(err);
-      return res.status(200).send(user);
+      return res.send(user.progress);
     }
   );
+});
+
+// Update a user's progress for a specific game
+router.patch("/users/:username/progress/games/:game", (req, res) => {
+  const { username } = req.params;
+  const { game } = req.params;
+  const { value } = req.body;
+  User.findOneAndUpdate(
+    { username },
+    { $set: { [`progress.games.${game}`]: value } },
+    { new: true },
+    (err, user) => {
+      if (err) return res.status(500).send(err);
+      return res.send(user.progress);
+    }
+  );
+});
+
+// Retrieve a list of all users
+router.get("/users", (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) return res.status(500).send(err);
+    return res.send(users);
+  });
 });
 
 module.exports = router;
