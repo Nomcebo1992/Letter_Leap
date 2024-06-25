bfafe7daa7d1887dc59631e2cdf69e4ecf3c5f15
Nomcebo1992 <snembambo@gmail.com>
2024-06-25 06:26:43 -0700

    index.js

---
+++ client/source/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LetterLeap from './LetterLeap';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LetterLeap />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
